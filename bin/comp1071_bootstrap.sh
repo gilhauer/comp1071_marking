8354b718c8a7fd5ba1f8afcb72b2b38abb1ab934
gil.i.hauer@gmail.com <4ju4s7ks>
2017-02-01 11:25:57 -0500

    Added bootstrap script

---
+++ bin/comp1071_bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Bootstrap the marking repository by installing tools and cloning the Github repository
+REPO=https://github.com/gilhauer/comp1071_marking.git
+
+# [ required packages]
+for pkg in git ruby; do
+    sudo apt-get -y install ${pkg}
+done
+
+# [ clone the repository as root ]
+(
+    cd /var; \
+    sudo git clone ${REPO}
+)
