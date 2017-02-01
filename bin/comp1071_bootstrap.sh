7f46fc61780051d6e8a25471e7156f66776758ff
gil.i.hauer@gmail.com <4ju4s7ks>
2017-02-01 15:11:56 -0500

    Added code to install 'inspec' package

--- bin/comp1071_bootstrap.sh
+++ bin/comp1071_bootstrap.sh
@@ -8,9 +8,19 @@ for pkg in git ruby; do
     sudo apt-get -y install ${pkg}
 done
 
-for gem in serverspec; do
-    gem install ${gem}
+# [ required gems -- needs some tools temporarily installed ]
+TMP_PKGS="gcc g++ ruby-dev"
+TMP_GEMS="rake"
+
+sudo apt-get -y install ${TMP_PKGS}
+sudo gem install ${TMP_GEMS} --http-proxy=http://172.26.6.91:8080
+
+for gem in inspec; do
+    sudo gem install ${gem}  --http-proxy=http://172.26.6.91:8080
 done
+
+sudo gem remove ${TMP_GEMS} --http-proxy=http://172.26.6.91:8080
+sudo apt-get -y remove ${TMP_PKGS}
 
 # [ clone the repository as root ]
 (
